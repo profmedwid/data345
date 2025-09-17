124e6a984e2a9235acd5edbb9a82353bff1720e4
profmedwid <prof.medwid@gmail.com>
2025-09-17 12:41:17 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="data345",
+    version="0.1.0",
+    description="Applied Linear Algebra tools for DATA 345",
+    author="Your Name",
+    packages=find_packages(),
+    python_requires=">=3.8",
+)
